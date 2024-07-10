abca3e09a65b982629d5a58adb4664be1cbe62c3
PeterMichael28 <enitanpeters28@gmail.com>
2024-07-10 10:44:01 +0100

    updated

--- src/components/FormPage/hvacs/HvacsFormComponents.jsx
+++ src/components/FormPage/hvacs/HvacsFormComponents.jsx
@@ -8,30 +8,59 @@ export const HVACS = ({ props }) => {
   const [value, setValue] = useState("");
   const data = [
     "Boiler",
-    "HVACs",
-    "Duct Vent Mini",
-    "Electrical Baseboard",
+    "Propane Furnace",
+    "Oil Furnace",
+    "Electric Furnace",
     "Furnace",
-    "Gas Heat",
-    "Geothermal",
-    "Heating",
-    "Heat Pumps",
-    "Oil Heat",
-    "Radiant Floor System",
-    "Thermostatic",
+    "Gas Boiler",
+    "Propane Boiler",
+    "Electric Boiler",
+    "Oil Boiler 1",
+    "Gas Furnace",
+    "Central Air",
+    "Heat Pump",
+    "Water Heater",
   ];
 
   const { allFields, updateFields } = useFormStore((state) => state);
 
   const handleClick = () => {
     if (!value) return;
-    updateFields({ ...allFields, hvacs: value });
+    updateFields({ ...allFields, AirSubType: value });
     props.onNext();
   };
 
   return (
     <div className="w-full">
       <FormHeader title={"HVACS"} subtitle={"What type of HVAC system best suits your needs?"} />
+
+      <div className="mt-7">
+        <div className="space-y-5">
+          {data.map((dat) => (
+            <FormSelectBox key={dat} active={value === dat} onClick={() => setValue(dat)} text={dat} />
+          ))}
+        </div>
+        <FormButton text="Continue" className="mt-7" onClick={handleClick} disabled={!value} />
+      </div>
+    </div>
+  );
+};
+
+export const AirType = ({ props }) => {
+  const [value, setValue] = useState("");
+  const data = ["Heating", "Cooling", "Heating and Cooling"];
+
+  const { allFields, updateFields } = useFormStore((state) => state);
+
+  const handleClick = () => {
+    if (!value) return;
+    updateFields({ ...allFields, AirType: value });
+    props.onNext();
+  };
+
+  return (
+    <div className="w-full">
+      <FormHeader title={"Air Type"} subtitle={"Your type of Air for this project?"} />
 
       <div className="mt-7">
         <div className="space-y-5">
